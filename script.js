9d72f9b8b4984d6bcaf26e2d09c9ff5abe67d2fc
Ryan Lok <rlok.pc@gmail.com>
2022-05-07 18:58:13 +0800

    Ensure proper capitalization of player choice in game result.

--- script.js
+++ script.js
@@ -4,10 +4,8 @@ let playerSelection = playerAnswer()
 function playerAnswer() {
     let answer = prompt("Rock, paper or scissors?")
 
-    console.log(answer)
-
     if (answer.toLowerCase() === "rock" || answer.toLowerCase() === "paper" || answer.toLowerCase() === "scissors") {
-        return answer
+        return answer.charAt(0).toUpperCase() + answer.slice(1).toLowerCase()
     } else if (answer === null) {
         alert("Please submit an answer.")
         playerAnswer()
@@ -17,8 +15,6 @@ function playerAnswer() {
     }
 
 }
-
-
 
 // 2. Randomly determine the computer's choice between "Rock" "Paper" and "Scissors"
 let computerSelection = computerPlay()
@@ -38,6 +34,21 @@ function computerPlay() {
     
 }
 
-console.log(computerSelection)
 // 3. Evaluate the different possible combinations of player and computer choice
 // 4. Determine whether player or computer has won depending on the combination
+let outcome = playRound(playerSelection, computerSelection)
+
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection === "Rock" && computerSelection === "paper" ||
+        playerSelection === "Paper" && computerSelection === "scissors" ||
+        playerSelection === "Scissors" && computerSelection === "rock") {
+            return `You lose! ${computerSelection} beats ${playerSelection}!`
+    } else if (playerSelection === "Rock" && computerSelection === "scissors" ||
+               playerSelection === "Paper" && computerSelection === "rock" ||
+               playerSelection === "Scissors" && computerSelection === "paper") {
+                   return `You win! ${playerSelection} beats ${computerSelection}!`
+               } else {
+                   return `It's a tie! ${playerSelection} ties with ${computerSelection}!`
+               }
+            }
+console.log(outcome)

a798c29758ad469204b48adacf230ce97dfeb920
Ryan Lok <rlok.pc@gmail.com>
2022-05-07 20:18:17 +0800

    Make game play 5 rounds, also ensure script prioritizes checking for null value for player answer.

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// // 1. Ask player to input their selection("Rock" "Paper" or "Scissors)
-let playerSelection = playerAnswer()
+// // 1. Function to ask player to input their selection("Rock" "Paper" or "Scissors)
+// let playerSelection = playerAnswer()
 
 function playerAnswer() {
     let answer = prompt("Rock, paper or scissors?")
 
-    if (answer.toLowerCase() === "rock" || answer.toLowerCase() === "paper" || answer.toLowerCase() === "scissors") {
-        return answer.charAt(0).toUpperCase() + answer.slice(1).toLowerCase()
-    } else if (answer === null) {
+    if (answer === null) {
         alert("Please submit an answer.")
         playerAnswer()
+    } else if (answer.toLowerCase() === "rock" || answer.toLowerCase() === "paper" || answer.toLowerCase() === "scissors") {
+        return answer.charAt(0).toUpperCase() + answer.slice(1).toLowerCase()
     } else {
         alert("Invalid answer. Please enter rock, paper or scissors.")
         playerAnswer()
@@ -16,8 +16,8 @@ function playerAnswer() {
 
 }
 
-// 2. Randomly determine the computer's choice between "Rock" "Paper" and "Scissors"
-let computerSelection = computerPlay()
+// 2. Function to randomly determine the computer's choice between "Rock" "Paper" and "Scissors"
+// let computerSelection = computerPlay()
 
 function computerPlay() {
     let randomNumber = Math.floor(Math.random() * 3)
@@ -36,7 +36,7 @@ function computerPlay() {
 
 // 3. Evaluate the different possible combinations of player and computer choice
 // 4. Determine whether player or computer has won depending on the combination
-let outcome = playRound(playerSelection, computerSelection)
+// let outcome = playRound(playerSelection, computerSelection)
 
 function playRound (playerSelection, computerSelection) {
     if (playerSelection === "Rock" && computerSelection === "paper" ||
@@ -51,4 +51,38 @@ function playRound (playerSelection, computerSelection) {
                    return `It's a tie! ${playerSelection} ties with ${computerSelection}!`
                }
             }
-console.log(outcome)
+
+
+// 5. Play a best out of five
+
+game()
+
+function game() {
+
+    let playerWins = 0
+    let computerWins = 0
+    let gameCounter = 0
+
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = playerAnswer();
+        let computerSelection = computerPlay();
+        let outcome = playRound(playerSelection, computerSelection);
+        gameCounter++;
+
+        if (outcome.charAt(4) === 'w') {
+            playerWins++;
+        } else if (outcome.charAt(4) === 'l') {
+            computerWins++;
+        }
+        console.log(`${outcome} You currently have ${playerWins} wins and the computer has ${computerWins} wins. You have played ${gameCounter} out of 5 games.`)
+    }
+
+    if (playerWins > computerWins) {
+        console.log(`Match over, you won!`);
+    } else if (computerWins > playerWins) {
+        console.log(`Match over, you lost!`);
+    } else {
+        console.log(`Match over, it's a draw!`);
+    }
+
+} 
